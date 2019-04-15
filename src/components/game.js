a7bac233b68dee3f2ac4f74621dd2a0c68c8de0e
mauriceokumu <maurice.okumu@thoughtworks.com>
2019-04-15 15:16:37 -0500

    Renders the game canvas

--- src/components/game.js
+++ src/components/game.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import * as PIXI from 'pixi.js';
+import PixiApp from '../gameboard/pixie'
 
-const Game = () => {
-    return (
-        <div>
-            <h2>The game canvas will be here</h2>
-        </div>
-    )
+class Game extends Component {
+    constructor() {
+        super();
+    }
+    render() {
+        const{ user } = this.props.auth;
+        return (
+            <div>
+                <h1>
+                    Game Canvas
+                </h1>
+                <p>{user.name}'s High Score : 0</p>
+                <PixiApp />
+            </div>
+        )
+    }
 }
 
-export default Game;
+const mapStateToProps = state => ({
+    auth: state.auth,
+})
+
+export default connect(mapStateToProps)(Game);
---
+++ src/gameboard/pixie.js
@@ -0,0 +1,12 @@
+import { Stage, Sprite } from '@inlet/react-pixi';
+import React, {Component } from 'react';
+
+const PixiApp = () => {
+   return (
+    <Stage>
+
+    </Stage>
+   ) 
+}
+
+export default PixiApp;
